4a4e954fa90ee09f31e23b263124819ee1c63369
huangruitian <576536299@qq.com>
2020-02-26 22:20:19 +0800

    finish server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
 import "reflect-metadata";// class-transformer 要用到这个库
 import Express from "express";
 import MovieRouter from "./routes/movieRoute";
+import UploadRoute from "./routes/movieRoute";
 
 // express、koa2 两个差不多
 const app = Express()
+
+// 请求静态文件，当请求/upload，访问 "public/upload"
+app.use("/upload", Express.static("public/upload"))
 
 // 配置中间件，用于解析请求消息体中的json格式
 app.use(Express.json())
@@ -11,5 +15,9 @@ app.use(Express.json())
 // 添加中间件, 请求这个地址，让 MovieRouter 处理
 // postman可以测试接口
 app.use("/api/movie", MovieRouter)
+
+// 文件上传
+// 通常情况下，服务器会提供一个统一的接口，处理上传的图片
+app.use("/api/upload", UploadRoute)
 
 app.listen(3000)
---
+++ server/src/routes/UploadRoute.ts
@@ -0,0 +1,66 @@
+import Express from "express";
+import multer from "multer"
+import path from "path"
+import { ResponseHelper } from "./ResponseHelper";
+const router = Express.Router()
+
+// 文件保存的配置
+const storage = multer.diskStorage({
+    destination: path.resolve(__dirname, '../../public/upload'),
+    filename(req, file, cb) {
+        // 文件名是什么，后缀名是什么？
+        const time = new Date().getTime()
+        const extname = path.extname(file.originalname)
+        const r = Math.random()
+        // 设置文件全称
+        cb(null, `${time}${r}${extname}` + Date.now())
+    }
+})
+
+// 限制的文件大小的配置
+const limits = {
+    fileSize: 1024 * 1024,
+}
+
+// 允许上传文件的后缀名
+const allowedExtensions = ['.jpg', '.png', '.gif', '.bmp', '.jiff']
+
+const upload = multer({
+    storage,
+    limits,
+    fileFilter(req, file, cb) {
+        const extname = path.extname(file.originalname)
+        if (allowedExtensions.includes(extname)) {
+            cb(null, true)
+        } else {
+            cb(new Error("文件类型不正确"))
+        }
+    }
+}).single("imgfile");
+
+// 客户端文件上传必须为post请求
+// 格式一般是form-data格式，放在请求体里 content-type: multipart/form-data
+// 服务器如何得到上传的图片？使用express中间件，npm i -D multer
+// 约定文件名为imgfile
+// 问题：
+// 1.如何设置上传的文件名字？根据客户端文件后缀名决定
+// 2.如何设置文件的大小？配置 limits
+// 3.如何限制文件后缀名？ 配置 fileFilter
+// 4.当发生错误时，如何响应给客户端；正确又如何响应？
+// 4.1 正确响应文件的路径，错误就响应错误消息
+// 5.静态资源如何被访问？
+router.post("/", (req, res) => {
+    // 先处理上传的文件
+    upload(req, res, (err) => {
+        if (err) {
+            // 发生错误
+            ResponseHelper.sendError(err.message, res)
+        } else {
+            // 一切都好
+            const url = `/upload/${req.file.filename}`
+            ResponseHelper.sendData(url, res)
+        }
+    })
+})
+
+export default router
